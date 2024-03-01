6f25219475a57c75b04a797671fbefc7e59f00ae
Luis Salas <52912999+Luis1908@users.noreply.github.com>
2024-03-01 01:37:38 -0300

    iniciando a header e separando o partial head

--- recipes/views.py
+++ recipes/views.py
@@ -4,6 +4,6 @@ from django.shortcuts import render
 # "Views" são funções que são chamadas pelos "paths", retornam caminhos de templates
 # Os templates são codigos HTML 
 def home(request):
-    return render(request, 'recipes/home.html', status="404", context={
+    return render(request, 'recipes/pages/home.html', status="404", context={
         "name": "Luis Salas"
     })
