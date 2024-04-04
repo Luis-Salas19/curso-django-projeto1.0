817bd1fd749a95fa1332a0fb1d9010d0072095d0
luisg <luisg2r7m0iorqu@gmail.com>
2024-04-04 18:33:41 -0300

    Injetando dados na lista da view Home

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 # Create your views here.
 
 # "Views" são funções que são chamadas pelos "paths", retornam caminhos de templates
 # Os templates são codigos HTML 
 def home(request):
     return render(request, 'recipes/pages/home.html', status="404", context={
-        "name": "Luis Salas"
+        "recipes": [make_recipe() for _ in range(10)],
     })
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', status="404", context={
-        "name": "Luis Salas"
+        "recipe": make_recipe()
     })
