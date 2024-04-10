90ddc5c3ebc13b0401d359f9f9375ebd012deddf
Luis-Salas19 <luisg2r7m0iorqu@gmail.com>
2024-04-10 16:59:57 -0300

    Nome único nas URLs e app_name para Namespace

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 # temos que importar as funções do file "views.py"
 from . import views
 
-
+app_name = 'recipes'
 
 # São requisições HTTP que aparecem na barra de pesquisa
 urlpatterns = [
     # "PATH" são caminhos que chamam funções que estão no file "views.py"
-    path('', views.home),
-    path('recipes/<int:id>/', views.recipe),
+    path('', views.home, name="home"),
+    path('recipes/<int:id>/', views.recipe, name="recipe"),
 ]
